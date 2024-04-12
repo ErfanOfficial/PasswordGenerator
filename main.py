8b6aaff6d9a5760ed8859e3272163308728d9b04
Unknown <Unknown@example.com>
2024-04-12 22:33:17 +0330

    Added password storage and retrieval functionality

--- main.py
+++ main.py
@@ -1,42 +1,88 @@
 import random
 import string
+import pickle
 
-# Asking the user for the length of the password | از کاربر درخواست می‌کنیم تا طول پسورد را مشخص کند
-password_length = int(input("How many characters do you want in your password?: "))
+def generate_password(length, type):
 
-# Asking the user for the type of characters they want in their password | از کاربر درخواست می‌کنیم تا نوع کاراکترهایی که می‌خواهد در پسورد خود داشته باشد را مشخص کند
-password_character_type = int(input("Here are the options you have:\n1. Only numbers.\n2. Only letters.\n3. Both numbers and letters\nPlease enter the number of your choice: "))
+    numbers = string.digits
+    letters = string.ascii_letters
+    both = string.digits + string.ascii_letters
 
-# Checking if the user's input is valid | بررسی می‌کنیم که آیا ورودی کاربر معتبر است
-while(password_character_type < 1 or password_character_type > 3):
-        password_character_type = int(input("Invalid input, please enter one of options 1, 2 or 3: "))
+    if (type == 1):
+        password = ""
+        for i in range(length):
+            password += random.choice(numbers)
+    elif (type == 2):
+        password = ""
+        for i in range(length):
+            password += random.choice(letters)
+    else:
+        password = ""
+        password += random.choice(numbers)
+        password += random.choice(letters)
+        for i in range(length - 2):
+            password += random.choice(both)
+        password = ''.join(random.sample(password, length))
+    return(password)
 
-# Defining the character sets | تعریف مجموعه‌های کاراکتر
-all_characters = string.ascii_letters + string.digits
-letters_only = string.ascii_letters
-numbers_only = string.digits
+def get_option():
+    option = int(input("Hello, what can I do for you?\n1. Generate password\n2. Display password list\nYour choice: "))
+    while(option < 1 or option > 2):
+        option = int(input("Please enter a valid value (1 or 2): "))
+    return(option)
 
-# Generating the password based on the user's choice | تولید پسورد بر اساس انتخاب کاربر
-if (password_character_type == 1):
-    password = ""
-    for item in range(password_length):
-        password += random.choice(numbers_only)
+def save_password():
+    save = int(input("Do you want save your password?\n1.YES\n2.NO\nYour choice: "))
+    while (save < 1 or save > 2):
+        save = int(input("Just enter 1 or 2\nYour choice: "))
+    return(save)
 
-elif (password_character_type == 2):
-    password = ""
-    for item in range(password_length):
-        password += random.choice(letters_only)
+option = get_option()
 
-elif (password_character_type == 3):
-    password = ""
-    password += random.choice(string.ascii_letters)
-    password += random.choice(string.digits)
-    for item in range(password_length - 2):
-        password += random.choice(all_characters)
+if (option == 1):
+    length = int(input("What should be the length of the characters?: "))
+    while(length < 2):
+        length = int(input("What should be the length of the characters? [You cannot choose a value less than two]: "))
 
-# Shuffling the characters in the password if the user chose both numbers and letters | اگر کاربر هر دو نوع کاراکتر را انتخاب کرده بود، کاراکترهای پسورد را مخلوط می‌کنیم
-if (password_character_type == 3):
-    password = ''.join(random.sample(password, len(password)))
+    type = int(input("What should be the character type?\n1.numbers\n2.letters\n3.both\nYour choice: "))
 
-# Printing the generated password | چاپ پسورد تولید شده
-print(password)
+    while (type > 3 or type < 1):
+        type = int(input("Incorrect input. Enter true value[1, 2 or 3]\nYour choice: "))
+
+    password = generate_password(length, type)
+    print("Your password = %s" %(password))
+
+    save = save_password()
+
+    user_passwords = {}
+
+    if (save == 1):
+        pass_name = input("Choose a name for your password: ")
+        while(len(pass_name) < 1):
+            pass_name = input("The name you choose must have at least 2 characters: ")
+        user_name = input("Enter a username: ")
+        while(len(pass_name) < 2):
+            pass_name = input("The username you choose must have at least 3 characters: ")
+        try:
+            with open("userpasswords.pkl", 'rb') as f:
+                user_passwords = pickle.load(f)
+        except FileNotFoundError:
+            pass
+
+        user_passwords[pass_name] = {"username":user_name, "password":password}
+
+        with open("userpasswords.pkl", 'wb') as f:
+            pickle.dump(user_passwords, f)
+        print("Successfully saved")
+    else:
+        print("Operation cancelled")
+
+else:
+    print("Here is the list of passwords you have saved:")
+    with open("userpasswords.pkl", 'rb') as f:
+        user_passwords = pickle.load(f)
+    for i in user_passwords.keys():
+        print(i)
+    select_password = input("Choose one of the above options: ")
+    show_password = user_passwords[select_password]
+    print("Name = {}\nUsername = {}\nPassword = {}".format(select_password, show_password["username"], show_password["password"]))
